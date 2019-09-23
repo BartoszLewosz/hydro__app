8e92d5b7a9aeaf21bbdd6e48ece2e4c756b343fa
Bartosz Lewosz <bartosz.lewosz@gmail.com>
2019-09-23 22:59:44 +0100

    Add JS code for add and remove water glass counter

--- src/js/main.js
+++ src/js/main.js
@@ -14,9 +14,38 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-// place your code below
+const add_water = document.querySelector('.glass__button--add-js');
+const remove_water = document.querySelector('.glass__button--remove-js');
+const count_water = document.querySelector('.glass__counter--js');
+const today_date = new Date().toISOString().slice(0,10);
+
+//console.log(key);
+
+//const key = new Date().toISOString().slice(0, 10)
 
 
-console.log(`Hello world!`);
+if (!localStorage.getItem(today_date)) {
+  localStorage.setItem(today_date, 0);
+  count_water.innerHTML = '0';
+}
+else {
+  count_water.innerHTML = localStorage.getItem(today_date);
+}
 
+add_water.addEventListener('click', (e) => {
+  const current_water = parseInt(localStorage.getItem(today_date));
+  if (current_water < 12) {
+    localStorage.setItem(today_date, parseInt(localStorage.getItem(today_date)) +1);
+    count_water.innerHTML = parseInt(localStorage.getItem(today_date));
+  }
+  
+})
+
+remove_water.addEventListener('click', (e) => {
+  const current_water = parseInt(localStorage.getItem(today_date));
+  if (current_water > 0) {
+    localStorage.setItem(today_date, localStorage.getItem(today_date) - 1);
+    count_water.innerHTML = parseInt(localStorage.getItem(today_date));
+  }
+})
 
